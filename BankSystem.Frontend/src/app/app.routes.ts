77f01678129e1fb1c9e4f499701e16aa3b60c454
Krzysztof <krzysztof.andrzej.jurkowski@gmail.com>
2024-01-22 20:50:35 +0100

    login page view

--- BankSystem.Frontend/src/app/app.routes.ts
+++ BankSystem.Frontend/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import {SignInPageComponent} from "./components/signin/sign-in-page/sign-in-page
 import {SignUpPageComponent} from "./components/signup/sign-up-page/sign-up-page.component";
 import {DashBoardPageComponent} from "./components/dashboard/dash-board-page/dash-board-page.component";
 import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
+import {PasswordInputPageComponent} from "./components/signin/password-input-page/password-input-page.component";
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'sign-in', component: SignInPageComponent },
+  { path: 'sign-in/:key', component: PasswordInputPageComponent },
   { path: 'sign-up', component: SignUpPageComponent },
   { path: 'dashboard', component: DashBoardPageComponent },
   { path: '**', component: NotFoundPageComponent }
