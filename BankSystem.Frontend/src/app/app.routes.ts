86b19d26b5504932c8d4628271aecef62c897cef
Krzysztof <krzysztof.andrzej.jurkowski@gmail.com>
2024-01-22 16:56:50 +0100

    init pages and routes

--- BankSystem.Frontend/src/app/app.routes.ts
+++ BankSystem.Frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomePageComponent} from "./components/home/home-page/home-page.component";
+import {SignInPageComponent} from "./components/signin/sign-in-page/sign-in-page.component";
+import {SignUpPageComponent} from "./components/signup/sign-up-page/sign-up-page.component";
+import {DashBoardPageComponent} from "./components/dashboard/dash-board-page/dash-board-page.component";
+import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'sign-in', component: SignInPageComponent },
+  { path: 'sign-up', component: SignUpPageComponent },
+  { path: 'dashboard', component: DashBoardPageComponent },
+  { path: '**', component: NotFoundPageComponent }
+];
